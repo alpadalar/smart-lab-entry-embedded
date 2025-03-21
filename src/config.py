44ce4599a747533b1f715756d3892278fb825786
alpadalar <alp.adalar@gmail.com>
2025-03-21 14:44:15 +0300

    Updated config file

--- src/config.py
+++ src/config.py
@@ -1,52 +1,56 @@
-# I2C ve Multiplexer Ayarları
+# I2C Multiplexer (PCA9548A) ayarları
 MULTIPLEXER_ADDR = 0x70
 I2C_BUS = 1
 
-# NFC Okuyucu Kanalları
-INSIDE_NFC_CHANNEL = 0
-OUTSIDE_NFC_CHANNEL = 1
-LCD_CHANNEL = 2
+# NFC Okuyucu (PN532) ayarları
+NFC_ADDR = 0x48
+NFC_IRQ = 17
+NFC_RESET = 27
 
-# LCD Ekran Ayarları
+# LCD Ekran ayarları
+LCD_CHANNEL = 0
 LCD_WIDTH = 20
 LCD_HEIGHT = 4
-LCD_ADDR = 0x27  # Varsayılan I2C adresi
+LCD_ADDR = 0x27
 
-# LED Şerit Ayarları
-LED_COUNT = 8
-INSIDE_LED_PIN = 18  # GPIO18
-OUTSIDE_LED_PIN = 23  # GPIO23
+# LED Şerit ayarları
+LED_COUNT = 30
+INSIDE_LED_PIN = 18
+OUTSIDE_LED_PIN = 12
 
-# Buzzer Pinleri
-INSIDE_BUZZER_PIN = 17  # GPIO17
-OUTSIDE_BUZZER_PIN = 27  # GPIO27
+# Buzzer ayarları
+INSIDE_BUZZER_PIN = 23
+OUTSIDE_BUZZER_PIN = 24
 
-# API Ayarları
-API_URL = "http://localhost:8080/api/access-portals/door-status"
-CONTROLLER_ID = "585285"
+# Relay ayarları
+RELAY_DURATION = 3  # saniye
 
-# Log Dosyası
-LOG_FILE = "access_log.txt"
+# API ayarları
+API_URL = "http://localhost:8000/api/access"
+CONTROLLER_ID = "RASPI_001"
 
-# LED Renkleri
+# Renk tanımlamaları
 COLORS = {
-    "WHITE": (255, 255, 255),
+    "OFF": (0, 0, 0),
+    "RED": (255, 0, 0),
     "GREEN": (0, 255, 0),
     "BLUE": (0, 0, 255),
-    "RED": (255, 0, 0),
-    "OFF": (0, 0, 0)
+    "WHITE": (255, 255, 255),
+    "YELLOW": (255, 255, 0),
+    "PURPLE": (255, 0, 255),
+    "CYAN": (0, 255, 255)
 }
 
-# Buzzer Süreleri (saniye)
-BUZZER_DURATIONS = {
-    "SUCCESS": 0.1,
-    "FAIL": 0.1,
-    "ERROR": 0.5
-}
-
-# LED Efekt Süreleri (saniye)
+# LED süreleri (saniye)
 LED_DURATIONS = {
-    "SUCCESS": 2,
-    "FAIL": 2,
-    "ERROR": 2
+    "SUCCESS": 1.0,
+    "FAIL": 0.5,
+    "ERROR": 0.3
+}
+
+# Buzzer süreleri (saniye)
+BUZZER_DURATIONS = {
+    "SUCCESS": 0.5,
+    "FAIL": 0.2,
+    "ERROR": 0.1
 } 
